89563d01eb5289d1d6fcd65366563510ad0e64b4
Creative Cowboys <josh@creativecowboys.co>
2026-03-16 15:11:19 -0400

    Make header logo bigger (h-20)

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar: React.FC<NavbarProps> = ({ cartCount, onCartOpen }) => {
           <img
             src="/squirrel made white stacked.png"
             alt="Squirrel Made"
-            className="h-14 w-auto"
+            className="h-20 w-auto"
           />
         </div>
 
