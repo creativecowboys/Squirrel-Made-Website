bd132507c1bad8691e075300a131f154fe5320ae
Creative Cowboys <josh@creativecowboys.co>
2026-03-16 10:17:32 -0400

    fix: update Instagram link to @squirrelmadeproducts

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,7 +19,7 @@ const Footer: React.FC = () => {
             Facebook
           </a>
           <a
-            href="https://www.instagram.com/squirrel.made"
+            href="https://www.instagram.com/squirrelmadeproducts"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="Instagram"
